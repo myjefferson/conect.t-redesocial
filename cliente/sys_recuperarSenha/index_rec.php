3ca05116618440dc9d22bc400a5bb3eb94a6a555
myjefferson <68786776+myjefferson@users.noreply.github.com>
2020-08-29 00:34:27 -0300

    Add files via upload

---
+++ cliente/sys_recuperarSenha/index_rec.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=cliice-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <title>CONEC.T SUPPORT - Recuperação de conta</title>
+    <link rel="icon" type="imagem/png" href="../../icones/conec.t-icon-head-web.png">
+    <link rel="stylesheet" href="./css/sysPassword.css" type="text/css">
+    <link rel="stylesheet" href="../style_cli/materialize/icons_materialize.css"> 
+    <link rel="stylesheet" href="../style_cli/css_cli/bootstrap.min.css" type="text/css">
+
+    <script type="text/javascript" src="../style_cli/js/jquery_ajax_2.1.3.min.js"></script>
+    <script type="text/javascript" src="../style_cli/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="../style_cli/js/bootstrap.min.js"></script>
+</head>
+    <body>
+        <div id="recuperar">
+            <img src="../style_cli/img_cli/logo.png" id="logo"><label id="support">SUPPORT</label>
+            <div class="container">
+                <div class="row">   
+                    <div class="box-center col-sm-12 col-md-12 col-lg-12 col-xs-12" align="center">
+                        <div class="box-form col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            <div class="box-enviar-email">   
+                                <span class="material-icons">lock</span>             
+                                <form class="col-sm-12 col-md-12 col-lg-12 col-xs-8" action="recuperar_sys.php" method="post">
+                                    <h3>Recuperação de conta</h3>
+                                    <h5>Cliente</h5>
+                                    <div>
+                                        <p id="txt-email">Email</p>
+                                        <input id="email" class="form-control" type="email" name="email_cli" placeholder="exemplo@email.com" autocomplete="off" require>
+                                        <p id="text_help">Um link de recuperação será enviado para seu e-mail</p>
+                                        <input class="form-control btn btn-primary" type="submit" name="btnEnviarEmail" value="Enviar">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <ul>
+				<li><a href="../login_cli.php">Entrar como cliente</a></li> 
+                <li><a href="../../desenvolvedor/login_dev.php">Entrar como desenvolvedor</a></li>            
+                <li><a href="../cadastro_cli.php">Cadastrar-se</a></li>
+                <li><a href="../../index.php">Sobre a CONEC.T</a></li>
+            </ul>
+        </footer>
+    </body>
+</html>
---
+++ cliente/sys_recuperarSenha/verify_user_cli.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>CONEC.T SUPPORT</title>
+    <meta name="viewport" content="width=cliice-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="icon" type="imagem/png" href="../../icones/conec.t-icon-head-web.png">
+    <link rel="stylesheet" href="./css/sysPassword.css" type="text/css">
+    <link rel="stylesheet" href="../style_cli/css_cli/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../style_cli/materialize/icons_materialize.css"> 
+    
+    <script type="text/javascript" src="../style_cli/js/jquery_ajax_2.1.3.min.js"></script>
+    <script type="text/javascript" src="../style_cli/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="../style_cli/js/bootstrap.min.js"></script>
+</head>
+    <body>
+        <div id="verify">
+            <?php 
+                session_start();
+                require '../../conexao_banco.php';
+                require "../lib/modais_info.php";   
+                
+                $id = $_GET['id'];
+                $pass = $_GET['pass'];
+                if(!empty($_GET['id']) && !empty($_GET['pass'])){    
+                    $queryCod = mysqli_query($conexao, "SELECT * FROM cod_recpassword WHERE id = '$id' AND codRandom = '$pass' ");
+                    if(mysqli_num_rows($queryCod) > 0){
+                        
+                        mysqli_query($conexao, "DELETE FROM cod_recpassword WHERE id = '$id' AND codRandom = '$pass' ");
+                    }else{
+                        header("Location: expirado.php");
+                        session_destroy();
+                    }   
+                }
+                
+            ?>
+
+            <img src="../style_cli/img_cli/logo.png" id="logo"><label id="support">SUPPORT</label>
+            <div class="container">
+                <div class="row">
+                    <div class="box-center col-sm-12 col-md-12 col-lg-12 col-xs-12" align="center">
+                        <div class="box-form col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            <div class="box-enviar-email">   
+                                <span class="material-icons">supervisor_account</span>             
+                                <form class="col-sm-12 col-md-12 col-lg-12 col-xs-8" action="alterar_senha.php?pass=<?php echo$_GET['pass']; ?>" method="POST">
+                                    <h3>Confirmação de usuário</h3>
+                                    <h5>Cliente</h5>
+                                    <p id="text_help"><b>Atenção:</b> </br>Preencha os campos corretamente para recuperar sua senha.</p>
+                                    <div>      
+                                        <p id="txt-email">Email</p>
+                                        <input class="form-control" type="email" name="email" placeholder="exemplo@email.com" required autocomplete="off">
+                                               
+                                        <div class="float-left col-6">
+                                            <p id="txt-email">Data de nascimento</p>
+                                            <input class="form-control" type="date" name="data-nasc" value="00-00-0000" required>
+                                        </div>
+                                            <input class="form-control btn btn-primary" type="submit" name="btnVerificar" value="Enviar">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
