f7ac44d18a38bd8fea01d076db4165b5c0195a74
myjefferson <68786776+myjefferson@users.noreply.github.com>
2020-08-29 00:32:49 -0300

    Add files via upload

---
+++ cliente/lib/itens_head.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">   
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<!--Logo head CONEC.T-->
+<link rel="icon" type="imagem/png" href="../icones/conec.t-icon-head-web.png">
+
+<!--Libs Interno-->
+<link rel="icon" type="imagem/png" href="../icones/conec.t-icon-head-web.png">
+<link rel="stylesheet" href="./style_cli/materialize/icons_materialize.css">  
+<link rel="stylesheet" href="./style_cli/css_cli/bootstrap.min.css" type="text/css">
+
+<!--Modais de Error e Ok-->
+<link rel="stylesheet" href="style_cli/css_cli/modais_errorAndConfir.css" type="text/css">
+
+<script type="text/javascript" src="./style_cli/js/jquery_ajax_2.1.3.min.js"></script>
+<script type="text/javascript" src="./style_cli/js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="./style_cli/js/bootstrap.min.js"></script>
